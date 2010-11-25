0b6d8f1f3fa22bc1ca47b2afa0e2e2fda2f00545
Jakh Daven <tuxcanfly@atlantis.(none)>
2010-11-26 01:50:09 +0530

    get info

--- contents/code/main.py
+++ contents/code/main.py
@@ -17,17 +17,44 @@ class Clementine(plasmascript.Applet):
         self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
         self.resize(200, 150)
         self.setHasConfigurationInterface(False)
-        self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
+        self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
         self.setLayout(self.layout)
-        self.label = Plasma.Label(self.applet)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.layout.addItem(self.label)
+
+        # album label
+        self.label_album = Plasma.Label(self.applet)
+        self.label_album.setAlignment(Qt.AlignCenter)
+        self.layout.addItem(self.label_album)
+        
+        # artist label
+        self.label_artist = Plasma.Label(self.applet)
+        self.label_artist.setAlignment(Qt.AlignCenter)
+        self.layout.addItem(self.label_artist)
+
+        # location label
+        self.label_location = Plasma.Label(self.applet)
+        self.label_location.setAlignment(Qt.AlignCenter)
+        self.layout.addItem(self.label_location)
+
+        # time label
+        self.label_time = Plasma.Label(self.applet)
+        self.label_time.setAlignment(Qt.AlignCenter)
+        self.layout.addItem(self.label_time)
+
+        # title label
+        self.label_title = Plasma.Label(self.applet)
+        self.label_title.setAlignment(Qt.AlignCenter)
+        self.layout.addItem(self.label_title)
+
         self.clementine_iface = self.get_dbus_object()
         self.current_track = self.clementine_iface.GetCurrentTrack()
         self.metadata = dict(self.clementine_iface.GetMetadata(self.current_track))
         self.track = Track(**self.metadata)
-        print self.track
-        #print self.clementine_iface.GetMetadata(self.current_track)
+
+        self.label_album.setText(self.track.album)
+        self.label_artist.setText(self.track.artist)
+        self.label_location.setText(self.track.location)
+        self.label_time.setText(str(self.track.time))
+        self.label_title.setText(self.track.title)
 
     def get_dbus_object(self):
         self.bus = dbus.SessionBus()
